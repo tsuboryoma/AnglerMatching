eab1087e8d598a54af340c2d04a583fb89235552
Ryoma Otsubo <2111140050a@ed.fuk.kindai.ac.jp>
2023-12-09 10:27:19 +0000

    fishing_trips_controllerの理解

--- app/controllers/fishing_trips_controller.rb
+++ app/controllers/fishing_trips_controller.rb
@@ -2,8 +2,10 @@ class FishingTripsController < ApplicationController
     
     
     def index
-        @fishing_trips = FishingTrip.all 
-        @is_organizer = {}
+        # @変数名 ⇦ インスタンス変数：ビューなどの他の場所でも利用することが出来る
+        # @変数名 = {} ⇦ 空のハッシュで初期化、ハッシュはキーと値のペアを格納するためのデータ構造
+        @fishing_trips = FishingTrip.all #FishingTripモデルの全てのレコードをDBから取得
+        @is_organizer = {} #募集の主催者が現在のユーザーかどうかの真偽値を保持
         @user_participation_status = {}
         @each_all_participations = {} #各募集に対する全参加申請
         @current_user_participations = {} #現在のユーザーの参加申請
@@ -11,16 +13,16 @@ class FishingTripsController < ApplicationController
         @fishing_trips.each do |trip|
            @is_organizer[trip.id] = trip.organizer == current_user
            @current_user_participations[trip.id] = trip.participations.where(user: current_user)
-            if @is_organizer[trip.id]
-                @each_all_participations[trip.id] = trip.participations
-            else
-                participation = trip.participations.find_by(user: current_user)
-                @user_participation_status[trip.id] = participation&.status
+            if @is_organizer[trip.id] #もし主催者であれば、
+                @each_all_participations[trip.id] = trip.participations #そのすべての参加情報を保存
+            else #主催者でなければ
+                participation = trip.participations.find_by(user: current_user) #そのユーザーに関連する参加情報を検索
+                @user_participation_status[trip.id] = participation&.status #見つかった参加情報のステータスを取得
             end
         end
         # パラメータで都道府県が指定されていれば、それに基づいてフィルタリング
-        if params[:location].present?
-          @fishing_trips = @fishing_trips.where(location: params[:location])
+        if params[:location].present? #リクエストのパラメータに'location'キーが存在し、かつそれに値が設定されている(空ではない)場合にtrueを返す
+          @fishing_trips = @fishing_trips.where(location: params[:location]) #location 属性がリクエストの location パラメータと一致する FishingTrip レコードのみを選択する
         end
     end
     
@@ -32,7 +34,7 @@ class FishingTripsController < ApplicationController
     
     def create
         @fishing_trip = FishingTrip.new(fishing_trip_params)
-        @fishing_trip.organizer = current_user #減税ログインしているユーザーを設定
+        @fishing_trip.organizer = current_user #現在ログインしているユーザーを設定
         if @fishing_trip.save
             redirect_to fishing_trips_path, notice: '募集が完了しました。'
         else
