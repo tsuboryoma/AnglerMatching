74e4455d65767783ae4b591424436095c494e861
Kento Higa <2111140057r@ed.fuk.kindai.ac.jp>
2023-12-13 09:01:00 +0000

    プロフィール登録、完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,16 +5,34 @@ class UsersController < ApplicationController
 
   def new
     @user=User.new
+    @user_profile=UserProfile.new
   end
   
   def create
-    if User.exists?(username: params[:user][:username])
-      redirect_to new_user_path
-    else
-      user=User.new(username: params[:user][:username],password: params[:user][:pass])
-      user.save
-      redirect_to top_login_path
-    end
+      if User.exists?(username: params[:user][:username])
+        redirect_to new_user_path
+      else
+        @user = User.new(user_params)
+        if @user.save
+          @user_profile = UserProfile.new(user_profile_params)
+          @user_profile.user_id = @user.id # UserとUserProfileを関連付ける
+          @user_profile.save
+          redirect_to top_login_path
+          # 保存が成功した場合の処理
+        else
+          redirect_to new_user_path
+        # 保存が失敗した場合の処理
+        end
+      end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :pass, user_profile_attributes: [:age, :biography, :region, :experience_years, :favorite_fishing_type])
+  end
+
+  def user_profile_params
+    params.require(:user).require(:user_profile).permit(:age, :biography, :region, :experience_years, :favorite_fishing_type)
   end
   
   def destroy
