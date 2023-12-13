6795961c25425b11bc581537174894a2a133f1ba
Ryoma Otsubo <2111140050a@ed.fuk.kindai.ac.jp>
2023-12-13 06:17:15 +0000

    bootstrap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+
 // import "@hotwired/turbo-rails"
 import { Turbo } from "@hotwired/turbo-rails"
 Turbo.session.drive = false
 import "controllers"
 import './custom/location_filter';
 import './custom/auto_reload';
+import 'bootstrap'
