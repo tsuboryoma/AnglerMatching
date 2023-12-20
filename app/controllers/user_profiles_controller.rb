5e62b356586af2e0b2657ae29d9ee99156fa4c42
Kento Higa <2111140057r@ed.fuk.kindai.ac.jp>
2023-12-20 07:52:10 +0000

    show,edit作成

--- app/controllers/user_profiles_controller.rb
+++ app/controllers/user_profiles_controller.rb
@@ -2,17 +2,22 @@ class UserProfilesController < ApplicationController
   def index
     @user_profiles = UserProfile.all
   end
+  
+  def show
+    @user_profile = UserProfile.find_by(user_id: params[:id])
+  end
 
   def edit
-    @user_profile = UserProfile.find(params[:id])
+    @user_profile = UserProfile.find_by(user_id: params[:id])
   end
   
   def update
-      @user_profile = UserProfile.find(params[:id])
-      if @user_profile.update(age: params[:id][:age],biography: params[:id][:biography],region: params[:id][:region],experience_years: params[:id][:experience_years],favorite_fishing_type: params[:id][:favorite_fishing_type])
-        redirect_to user_profiles_path
+      @user_profile = UserProfile.find_by(user_id: params[:id])
+      if @user_profile.update(age: params[:user_profile][:age],biography: params[:user_profile][:biography],region: params[:user_profile][:region],experience_years: params[:user_profile][:experience_years],favorite_fishing_type: params[:user_profile][:favorite_fishing_type])
+        @user_profile.save
+        redirect_to user_profile_path
       else
-        render 'edit'
+        render "edit"
       end
   end
 end
