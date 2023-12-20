8d6dd62fac1379e2b510efa54823f4a55a56a202
Ryoma Otsubo <2111140050a@ed.fuk.kindai.ac.jp>
2023-12-20 06:08:33 +0000

    header作成

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 //= require jquery3
+//= require jquery_ujs
 //= require popper
+//= require bootstrap
 //= require bootstrap-sprockets
 
 // import "@hotwired/turbo-rails"
@@ -9,4 +11,5 @@ Turbo.session.drive = false
 import "controllers"
 import './custom/location_filter';
 import './custom/auto_reload';
+import './custom/btn_control';
 import 'bootstrap'
