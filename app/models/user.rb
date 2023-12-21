1dcccf405dd423de73915dd98aa222ba92db06c1
Ryoma Otsubo <2111140050a@ed.fuk.kindai.ac.jp>
2023-12-21 11:48:01 +0000

    m

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
     has_secure_password
     has_many :organized_fishing_trips, class_name: 'FishingTrip', foreign_key: 'organizer_id', dependent: :destroy
     has_many :participations, dependent: :destroy
-    has_one :user_profile
+    has_one :user_profile, dependent: :destroy
 end
